65670d084c5111d9fa370efc7cbed26764acd129
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-06-15 16:11:40 +0500

    lesson_51: - Создание маршрутов из ДЗ - Создание представлений из ДЗ - Добавление набора данных из ДЗ

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -1,9 +1,16 @@
 # barbershop/urls.py
+
+### Таблица маршрутов, представлений и шаблонов
+
+
 from django.contrib import admin
 from django.urls import path
-from core.views import landing
+from core.views import landing, thanks, orders_list, order_detail
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", landing),
+    path("thanks/", thanks),
+    path("orders/", orders_list),
+    path("orders/<int:order_id>/", order_detail),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,33 @@
 # core/views.py
 from django.shortcuts import render, HttpResponse
 
+
 def landing(request):
+    """
+    Отвечает за маршрут '/'
+    """
     return HttpResponse("<h1>Главная страница</h1>")
+
+
+def thanks(request):
+    """
+    Отвечает за маршрут 'thanks/'
+    """
+    return HttpResponse("<h1>Спасибо за заказ!</h1>")
+
+
+def orders_list(request):
+    """
+    Отвечает за маршрут 'orders/'
+    """
+    return HttpResponse("<h1>Список заказов</h1>")
+
+
+def order_detail(request, order_id):
+    """
+    Отвечает за маршрут 'orders/<int:order_id>/'
+    :param request: HttpRequest
+    :param order_id: int (номер заказа)
+    """
+    return HttpResponse(f"<h1>Детали заказа {order_id}</h1>")
+
