ab0c5d73485643d51d94ad0f161a11a049eb9666
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-07-12 17:27:53 +0500

    lesson_58: - Посчет количество услуг оказываемых мастером через annotate - агрегатную функцию Sum и модификация шаблона главной страницы

--- core/views.py
+++ core/views.py
@@ -5,14 +5,15 @@ from django.contrib import messages
 from .data import orders
 from .models import Order, Master, Service
 from .forms import OrderForm
-from django.db.models import Q
+from django.db.models import Q, Count
 
 
 def landing(request):
     """
     Отвечает за маршрут '/'
     """
-    masters = Master.objects.prefetch_related("services").all()
+    # masters = Master.objects.prefetch_related("services").all()
+    masters = Master.objects.prefetch_related('services').annotate(num_services=Count('services'))
     
     # Получаем все услуги отдельным запросом
     services = Service.objects.all()
