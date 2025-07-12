5f3fc450978616bf700f1d0fb4b7966cbfa7f8b3
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-07-12 17:47:32 +0500

    lesson_58: - Детальное представление услуги (шаблон и вью) - Модификация карточки услуги - добавили кнопку на детальное представление услуги

--- core/views.py
+++ core/views.py
@@ -107,18 +107,11 @@ def order_detail(request, order_id):
     :param request: HttpRequest
     :param order_id: int (номер заказа)
     """
-    order = [order for order in orders if order["id"] == order_id]
-    try:
-        order = order[0]
-        context = {
-            "order": order,
-            "my_fariable": "Hello, world!",
-        }
-    except IndexError:
-        return HttpResponse("<h1>Заказ не найден</h1>", status=404)
+    order = Order.objects.get(id=order_id)
 
-    else:
-        return render(request, "order_detail.html", context=context)
+    context = {"order": order}
+
+    return render(request, "order_detail.html", context=context)
 
 
 def order_page(request):
