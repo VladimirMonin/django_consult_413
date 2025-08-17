b36620a49ef5b39ae4243a9b10cae3259c626990
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-08-17 12:17:36 +0500

    lesson_68: - Заготовки для сброса пароля - Маршруты - Формы - Болванки view

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,11 @@
 # users/forms.py
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, PasswordChangeForm
+from django.contrib.auth.forms import (
+    AuthenticationForm,
+    UserCreationForm,
+    PasswordChangeForm,
+    SetPasswordForm,
+    PasswordResetForm,
+)
 from django import forms
 
 # get_user_model - получает модель пользователя автоматически, и нам не страшно если мы поменяем модель пользователя в будущем
@@ -8,11 +14,36 @@ from django.contrib.auth import get_user_model
 # Мы поменяли модель пользователя, но так как используется get_user_model нам не надо ничего менять!)
 user_model = get_user_model()
 
+
+class CustomSetPasswordForm(SetPasswordForm):
+    """
+    Форма установки нового пароля (шаг 5)
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({"class": "form-control"})
+
+
+class CustomPasswordResetForm(PasswordResetForm):
+    """
+    Форма ввода емейла для сброса пароля (шаг 2)
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({"class": "form-control"})
+
+
 class CustomPasswordChangeForm(PasswordChangeForm):
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            for field_name, field in self.fields.items():
-                field.widget.attrs.update({"class": "form-control"})
+    """
+    Форма смены пароля (например в личном кабинете) - когда знаем старый пароль
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({"class": "form-control"})
+
 
 class CustomRegisterForm(UserCreationForm):
     class Meta:
--- users/views.py
+++ users/views.py
@@ -1,10 +1,57 @@
 from django.shortcuts import redirect
-from .forms import CustomRegisterForm, CustomLoginForm, CustomPasswordChangeForm
+from .forms import (
+    CustomRegisterForm,
+    CustomLoginForm,
+    CustomPasswordChangeForm,
+    CustomSetPasswordForm,
+    CustomPasswordResetForm,
+)
 from django.views.generic.edit import CreateView
-from django.contrib.auth.views import LogoutView, LoginView, PasswordChangeView
+from django.contrib.auth.views import (
+    LogoutView,
+    LoginView,
+    PasswordChangeView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
 from django.contrib import messages
 from django.contrib.auth import login
 from django.urls import reverse_lazy
+
+
+class CustomPasswordResetView(PasswordResetView):
+    """
+    1. Начало сброса пароля. Человек вводит емейл для сброса
+    """
+
+    pass
+
+
+class CustomPasswordResetDoneView(PasswordResetDoneView):
+    """
+    2. Загляните в Emeil - вам ушло письмо с инструкциями
+    """
+
+    pass
+
+
+class CustomPasswordResetConfirmView(PasswordResetConfirmView):
+    """
+    3. Вью где отображается форма восстановление пароля (можно попасть только через платформу 9 3\4
+    ) - комбо <uidb64> + <token>
+    """
+
+    pass
+
+
+class CustomPasswordResetCompleteView(PasswordResetCompleteView):
+    """
+    Ура! Вы сбросили пароль!
+    """
+
+    pass
 
 
 class CustomPasswordChangeView(PasswordChangeView):
@@ -15,11 +62,12 @@ class CustomPasswordChangeView(PasswordChangeView):
     def form_valid(self, form):
         messages.success(self.request, "Пароль успешно изменен!")
         return super().form_valid(form)
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["operation_type"] = "Смена пароля"
         return context
+
 
 class CustomRegisterView(CreateView):
     form_class = CustomRegisterForm
